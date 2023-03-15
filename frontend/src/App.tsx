3df6dfa2df730380d04ad3f2d47937e28e5bdd8d
yoorimseo <seou1225@naver.com>
2023-03-16 01:45:28 +0900

    build: 프로젝트 초기 세팅 #1

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { ChakraProvider, Box, Text, Grid, theme } from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box
+      textAlign='center'
+      fontSize='xl'
+    >
+      <Grid
+        minH='100vh'
+        p={3}
+      >
+        <ColorModeSwitcher justifySelf='flex-end' />
+        <Text>Boardgame-rental</Text>
+      </Grid>
+    </Box>
+  </ChakraProvider>
+);
