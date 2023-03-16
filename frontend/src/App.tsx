d0d41f2194288469ba50c5ff1a9b3dd22efc6da1
yoorimseo <seou1225@naver.com>
2023-03-16 21:49:12 +0900

    build: prettier & eslint 설정 #1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,9 @@ import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box
-      textAlign='center'
-      fontSize='xl'
-    >
-      <Grid
-        minH='100vh'
-        p={3}
-      >
-        <ColorModeSwitcher justifySelf='flex-end' />
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
         <Text>Boardgame-rental</Text>
       </Grid>
     </Box>
