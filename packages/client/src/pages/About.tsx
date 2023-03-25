f329b566420c9b3e47caa5ce1d40310d04cbc698
ParkEunwoo <dmsdn960@gmail.com>
2023-03-25 22:45:16 +0900

    feat: 로그인 여부에 따라 시작하기 링크 변경

--- packages/client/src/pages/About.tsx
+++ packages/client/src/pages/About.tsx
@@ -2,8 +2,10 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { Flex, Heading, Text, Button } from '@chakra-ui/react';
 import Header from '../components/Header';
+import { useAuthContext } from '../AuthProvider';
 
 const About = () => {
+  const { user } = useAuthContext();
   return (
     <>
       <Header />
@@ -14,7 +16,7 @@ const About = () => {
         <Text fontSize="lg" mt={9} mx={9}>
           갖고 있는 보드게임 목록을 만들고 공유해보세요!
         </Text>
-        <Link to="login">
+        <Link to={user ? `/list/${user.uid}` : '/login'}>
           <Button size="md" width="200px" colorScheme="blue" mt={9}>
             시작하기
           </Button>
