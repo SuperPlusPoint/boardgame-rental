b71e49dd1f32d3ea9ad24093a46664e78579ea9c
yoorimseo <seou1225@naver.com>
2023-03-26 01:30:42 +0900

    1차 QA 마크업 검색 결과 조건 변경 #49

--- packages/client/src/components/EditableBoardGameList.tsx
+++ packages/client/src/components/EditableBoardGameList.tsx
@@ -16,15 +16,9 @@ const EditableBoardGameList: React.FC<EditableBoardGameListProps> = ({
   return (
     <Box w="100%" maxHeight="68vh" overflow="scroll" mt={2}>
       {boardGameList.length === 0 ? (
-        filter.length !== 0 ? (
-          <Text mt={5} textAlign="center">
-            검색 결과가 없습니다.
-          </Text>
-        ) : (
-          <Text mt={5} textAlign="center">
-            보드게임을 추가해주세요.
-          </Text>
-        )
+        <Text mt={5} textAlign="center">
+          {!filter ? '보드게임을 추가해주세요.' : '검색 결과가 없습니다.'}
+        </Text>
       ) : (
         boardGameList.map((boardGame) => (
           <EditableListItem
