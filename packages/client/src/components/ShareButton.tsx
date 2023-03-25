3b0844285abb71b77b6ca8b88298c4abbd6f9074
YoorimSeo <seou1225@naver.com>
2023-03-25 21:11:40 +0900

    feat: Clipboard API를 사용하여 링크 복사 기능 구현 #9 (#44)

--- packages/client/src/components/ShareButton.tsx
+++ packages/client/src/components/ShareButton.tsx
@@ -8,11 +8,27 @@ import {
   ModalHeader,
   ModalOverlay,
   useDisclosure,
+  useToast,
 } from '@chakra-ui/react';
 import { LinkIcon } from '@chakra-ui/icons';
 
 const ShareButton = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const toast = useToast();
+
+  const copyClipboard = async (
+    text: string,
+    successAction?: () => void,
+    failAction?: () => void
+  ) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      return successAction && successAction();
+    } catch (error) {
+      return failAction && failAction();
+    }
+  };
+
   return (
     <>
       <IconButton
@@ -39,7 +55,30 @@ const ShareButton = () => {
           <ModalHeader>공유하기</ModalHeader>
           <ModalCloseButton />
           <ModalBody display="flex" flexDirection="column" gap={2} mb={3}>
-            <Button colorScheme="blue" variant="outline" mr={3}>
+            <Button
+              colorScheme="blue"
+              variant="outline"
+              mr={3}
+              onClick={() =>
+                copyClipboard(
+                  window.location.href,
+                  () =>
+                    toast({
+                      title: '링크가 클립보드에 저장되었습니다.',
+                      status: 'success',
+                      duration: 1000,
+                      isClosable: true,
+                    }),
+                  () =>
+                    toast({
+                      title: '다시 시도해주세요.',
+                      status: 'success',
+                      duration: 1000,
+                      isClosable: true,
+                    })
+                )
+              }
+            >
               링크로 공유하기
             </Button>
             <Button colorScheme="yellow" mr={3}>
